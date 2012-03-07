c9d667c6d37c5327a09f3f0cbb2bd31cc4da6e94
Chase Lee <clee231@uic.edu>
2012-03-07 16:06:51 -0600

    Updated Yellow-Border for Chrome

--- jscript/custom.js
+++ jscript/custom.js
@@ -4,7 +4,7 @@
 
 $(window).load(function () {
 $('body').append('<div id="theme-arrow" style="border-left: 5px solid transparent;border-right: 5px solid transparent;border-top: 5px solid black;cursor: pointer;font-size: 12px;position: absolute;right: 5em;top: 1em; width: 0;">Theme Changer</div>');
-$('body').append('<div id="theme-content" style="background-color: #DDDDDD;display: none;height: 75%;position: absolute;right: 0;top: 5em;width: 100px;"><ul style="list-style: none outside none; margin: 10px -25px; cursor: pointer;"><li style="padding-bottom: 10px;"><img src="./images/themer/theme-btn-1.gif" style="height:70px;" alt="Default Theme"/></li><li style="padding-bottom: 10px;"><img src="./images/themer/theme-btn-2.gif" style="height:70px;" alt="Gray Theme"/></li><li style="padding-bottom: 10px;"><img src="./images/themer/theme-btn-3.gif" style="height:70px;" alt="Yellow Theme"/></li></ul></div>');
+$('body').append('<div id="theme-content" style="background-color: #DDDDDD;display: none;height: 75%;position: absolute;right: 0;top: 5em;width: 100px;"><ul style="list-style: none outside none; margin: 10px -25px; cursor: pointer;"><li style="padding-bottom: 10px;"><img src="./images/themer/theme-btn-1.gif" style="height:70px;" alt="Default Theme"/></li><li style="padding-bottom: 10px;"><img src="./images/themer/theme-btn-2.gif" style="height:70px;" alt="Gray Theme"/></li><li style="padding-bottom: 10px;"><img src="./images/themer/theme-btn-3.gif" style="height:70px;" alt="Yellow Theme"/></li><li style="padding-bottom: 10px;"><img src="./images/themer/theme-btn-3.gif" style="height:70px;" alt="Yellow-Border Theme"/></li></ul></div>');
 
 $('#theme-arrow').click(function() {
 		$('#theme-content').slideToggle();
@@ -17,6 +17,9 @@ $('#theme-content li').eq(1).click(function() {
 });
 $('#theme-content li').eq(2).click(function() {
 	$('link[rel="StyleSheet"]').eq(0).attr('href', $('link[rel="StyleSheet"]').eq(0).attr('href').replace(/\/[^\/]+$/, '') + '/yellow.css');
+});
+$('#theme-content li').eq(3).click(function() {
+	$('link[rel="StyleSheet"]').eq(0).attr('href', $('link[rel="StyleSheet"]').eq(0).attr('href').replace(/\/[^\/]+$/, '') + '/yellow-border.css');
 });
 
 });
